fb11cf7ac92bf884f8c432771d21610a5a04aaf4
Abhishek Kumar <abhishekumar21112001@gmail.com>
2026-06-06 17:34:24 +0530

    Added page table and physical address translation

--- paging/paging.cpp
+++ paging/paging.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <unordered_map>
+
 using namespace std;
 
 class Paging
 {
+private:
+    unordered_map<int, int> pageTable;
+
 public:
+    Paging()
+    {
+        pageTable[0] = 3;
+        pageTable[1] = 1;
+        pageTable[2] = 5;
+        pageTable[3] = 2;
+    }
+
     void translateAddress()
     {
         int logicalAddress;
@@ -23,6 +36,23 @@ public:
 
         cout << "Offset : "
              << offset << endl;
+
+        if(pageTable.find(pageNumber) == pageTable.end())
+        {
+            cout << "\nPage not found in page table!" << endl;
+            return;
+        }
+
+        int frameNumber = pageTable[pageNumber];
+
+        int physicalAddress =
+            frameNumber * pageSize + offset;
+
+        cout << "\nFrame Number : "
+             << frameNumber << endl;
+
+        cout << "Physical Address : "
+             << physicalAddress << endl;
     }
 };
 
